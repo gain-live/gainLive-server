9924caa10e5930bb7a0c76f2becab44bb26ff8d3
Eippie8735 <chyuchyu600@gmail.com>
2024-11-04 14:47:05 +0900

    nodemon

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// app.use((req,res,next)=>{
-//     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-//     error.status = 404;
-//     next(error);
-// })
+app.use((req,res,next)=>{
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
 
 const app = express();
 app.set('port',process.env.PORT || 8000);
