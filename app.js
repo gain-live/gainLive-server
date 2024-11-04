ec736c8dae288deb49c7a818116a03285a0a0e4c
Eippie8735 <chyuchyu600@gmail.com>
2024-11-04 15:38:11 +0900

    app.js 주석 삭제

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// const express = require('express')
-// const dotenv = require('dotenv');
-
-
-
-// // app.use((req,res,next)=>{
-// //     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-// //     error.status = 404;
-// //     next(error);
-// // });
-
-// const app = express();
-// app.set('port',process.env.PORT || 8000);
-
-// app.use((err,req,res,next)=>{
-//     res.locals.message = err.message;
-//     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
-
-
 const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
