79765d10f976e5cee0d8017155c9be3e0d9a279b
Eippie8735 <chyuchyu600@gmail.com>
2024-11-14 14:24:05 +0900

    Update user.js
    auth 세션 로그인 부분 삭제

--- models/user.js
+++ models/user.js
@@ -3,19 +3,6 @@ const Sequelize = require('sequelize');
 class User extends Sequelize.Model {
   static initiate(sequelize) {
     User.init({
-      email: {
-        type: Sequelize.STRING(40),
-        allowNull: true,
-        unique: true,
-      },
-      nick: {
-        type: Sequelize.STRING(15),
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING(100),
-        allowNull: true,
-      },
       provider: {
         type: Sequelize.ENUM('local', 'kakao'),
         allowNull: false,
